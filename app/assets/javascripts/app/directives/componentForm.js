ea8c838fc82403bd4d9ed7eaa1051cd3df10f137
Daniel Brad <danieltbrad@gmail.com>
2016-08-14 18:56:26 -0700

    created link function for componentForm directive and cleaned up the table in its template

--- app/assets/javascripts/app/directives/componentForm.js
+++ app/assets/javascripts/app/directives/componentForm.js
@@ -2,9 +2,31 @@ function componentForm () {
   return {
     scope: {comp: "="},
     replace: true,
-    templateUrl: 'app/views/directive_templates/componentForm.html'
-  }
-}
+    templateUrl: 'app/views/directive_templates/componentForm.html',
+    link: function (scope, element, attrs) {
+
+      var sel = element[0].querySelector('select');
+
+      sel.addEventListener('change', function () {
+
+        if (scope.selectedComponent) {
+          scope.comp.brand = scope.selectedComponent.brand
+          scope.comp.name = scope.selectedComponent.name
+          scope.comp.price = scope.selectedComponent.price
+          scope.comp.url = scope.selectedComponent.url
+          }
+
+        else {
+          scope.comp.brand = ""
+          scope.comp.name = ""
+          scope.comp.price = ""
+          scope.comp.url = ""
+          }
+	      scope.$apply();
+      });
+    }
+  };
+};
 
 angular
   .module('app')
