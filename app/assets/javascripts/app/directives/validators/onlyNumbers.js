d680410d644d7a435fef77e06f9d2de632c044a7
Daniel Brad <danieltbrad@gmail.com>
2016-08-14 19:38:16 -0700

    added only-numbers custom validator to price input in componentForm directive

---
+++ app/assets/javascripts/app/directives/validators/onlyNumbers.js
@@ -0,0 +1,15 @@
+function onlyNumbers() {
+  return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function (scope, element, attrs, ngModel) {
+        ngModel.$validators.onlyNumbers = function (value) {
+          return (/^\-?\d+$/).test(value);
+  			};
+  		}
+  	}
+  }
+
+angular
+    .module('app')
+    .directive('onlyNumbers', onlyNumbers);
