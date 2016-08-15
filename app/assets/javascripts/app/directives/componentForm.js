57e3aadd53ea85e949fc5f31c1901cf8311c1c58
Daniel Brad <danieltbrad@gmail.com>
2016-08-14 21:06:47 -0700

    extracted the chunk of componentForm template sitting in an ng-if into its own directive

--- app/assets/javascripts/app/directives/componentForm.js
+++ app/assets/javascripts/app/directives/componentForm.js
@@ -6,24 +6,24 @@ function componentForm () {
     link: function (scope, element, attrs) {
 
       var sel = element[0].querySelector('select');
-
       sel.addEventListener('change', function () {
-
         if (scope.selectedComponent) {
           scope.comp.brand = scope.selectedComponent.brand
           scope.comp.name = scope.selectedComponent.name
           scope.comp.price = scope.selectedComponent.price
           scope.comp.url = scope.selectedComponent.url
           }
-
         else {
           scope.comp.brand = ""
           scope.comp.name = ""
           scope.comp.price = undefined
           scope.comp.url = ""
-          }
+        }
 	      scope.$apply();
       });
+
+      debugger;
+
     }
   };
 };
---
+++ app/assets/javascripts/app/directives/validators/allOrNone.js
@@ -0,0 +1,14 @@
+function allOrNone(DataService) {
+  return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function (scope, element, attrs, ngModel) {
+    
+
+  		}
+  	}
+  }
+
+angular
+    .module('app')
+    .directive('allOrNone', allOrNone);
