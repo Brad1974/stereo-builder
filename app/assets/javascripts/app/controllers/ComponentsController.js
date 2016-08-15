3c83c7264ab5dd7ec720e08631362c9d1e9c3243
Daniel Brad <danieltbrad@gmail.com>
2016-08-15 10:19:14 -0700

    added price range filter to components index

--- app/assets/javascripts/app/controllers/ComponentsController.js
+++ app/assets/javascripts/app/controllers/ComponentsController.js
@@ -1,10 +1,19 @@
-function ComponentsController(components, $filter) {
+function ComponentsController($scope, components, $filter) {
 
   var ctrl = this
   ctrl.components = components.data
+  ctrl.filteredComponents = ctrl.components
   ctrl.sortColumn = 'name';
   ctrl.sortReverse = false;
 
+  ctrl.withinRange = function(){
+    ctrl.components = components.data.filter(function(x){ if (ctrl.min < x.price == x.price < ctrl.max){ return x }})
+  }
+
+  ctrl.reset = function(){
+    ctrl.components= components.data
+
+  }
 
 
 }
