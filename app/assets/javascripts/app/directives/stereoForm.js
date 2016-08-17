5c9878cf81e5b314fef046eb7c6745139e67df53
Daniel Brad <danieltbrad@gmail.com>
2016-08-17 11:12:03 -0700

    hid edit button when view is used for new stereo and created validation for when no components are selected or entered in

--- app/assets/javascripts/app/directives/stereoForm.js
+++ app/assets/javascripts/app/directives/stereoForm.js
@@ -2,7 +2,15 @@ function stereoForm () {
   return {
     scope: {stereo: "=", add: "&"},
     replace: true,
-    templateUrl: 'app/views/directive_templates/stereoForm.html'
+    templateUrl: 'app/views/directive_templates/stereoForm.html',
+    controller: function($scope) {
+      $scope.daniel = function(){
+        if ( $scope.stereo.component_attributes.filter(function(c){return c.name != ""}).length > 0  )
+          { return false }
+        else
+          { return true  }
+        }
+    }
   }
 }
 
