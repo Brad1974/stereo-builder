4bf45dd380e1de02837f737b183a9d66b543d4dd
Daniel Brad <danieltbrad@gmail.com>
2016-08-18 14:32:53 -0700

    toggled edit form title, refactored nameTaken validator to cover both stereo profile as well as model name fields, eliminated needless manualEntry directive

--- app/assets/javascripts/app/directives/validators/nameTaken.js
+++ app/assets/javascripts/app/directives/validators/nameTaken.js
@@ -5,17 +5,39 @@ function nameTaken(DataService) {
       link: function (scope, element, attrs, ctrl) {
         element[0].addEventListener('change', function(){
           value = element[0].value
-          DataService.getComponents()
-          .then(function(result){
-            if ( (result.data.filter(function(c){return c.name === value })).length > 0   )
-            { ctrl.$setValidity('nameTaken', false)}
-            else { ctrl.$setValidity('nameTaken', true) }
-          })
+          name = element[0].name
+          if (name === 'stereoName')
+
+          {
+
+            DataService.getStereos()
+                .then(function(result){
+                  if ( (result.data.filter(function(x){return x.name === value })).length > 0   )
+
+                    { ctrl.$setValidity('nameTaken', false)}
+
+                  else { ctrl.$setValidity('nameTaken', true) }
+                });
+          }
+
+          else
+
+          {
+            DataService.getComponents()
+            .then(function(result){
+              if ( (result.data.filter(function(x){return x.name === value })).length > 0   )
+
+                { ctrl.$setValidity('nameTaken', false)}
+
+              else { ctrl.$setValidity('nameTaken', true) }
+            });
+          }
+
         });
 
+        }
   		}
   	}
-  }
 
 angular
     .module('app')
