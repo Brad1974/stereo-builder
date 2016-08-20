bfc510ebe35dc01fb6125c77932e0bddfabcfa05
Daniel Brad <danieltbrad@gmail.com>
2016-08-20 14:15:15 -0700

    added stereo form show/hide toggle

--- app/assets/javascripts/app/directives/stereosIndexDisplay.js
+++ app/assets/javascripts/app/directives/stereosIndexDisplay.js
@@ -4,7 +4,6 @@ function stereosIndexDisplay () {
     replace: true,
     templateUrl: 'app/views/directive_templates/stereosIndexDisplay.html',
     controller: function($scope) {
-      debugger;
       $scope.receiver = $scope.stereo.component_attributes.filter(function(x){return x.category == "receiver"})[0] || "";
       $scope.speaker = $scope.stereo.component_attributes.filter(function(x){return x.category == "speaker"})[0] || "";
       $scope.media_player = $scope.stereo.component_attributes.filter(function(x){return x.category == "media_player"})[0] || "";
