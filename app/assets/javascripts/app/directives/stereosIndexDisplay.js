742f980c5bdf1aced92d990b9763b241c233f153
Daniel Brad <danieltbrad@gmail.com>
2016-08-20 13:59:16 -0700

    refactored stereosIndexDisplay directive

--- app/assets/javascripts/app/directives/stereosIndexDisplay.js
+++ app/assets/javascripts/app/directives/stereosIndexDisplay.js
@@ -1,15 +1,13 @@
 function stereosIndexDisplay () {
   return {
-    scope: {},
+    scope: { stereo: "="},
     replace: true,
     templateUrl: 'app/views/directive_templates/stereosIndexDisplay.html',
-    bindToController: { stereo: "="},
-    controllerAs: 'ctrl',
-    controller: function() {
-      ctrl = this
-      ctrl.receiver = ctrl.stereo.component_attributes.filter(function(x){return x.category == "receiver"})[0];
-      ctrl.speaker = ctrl.stereo.component_attributes.filter(function(x){return x.category == "speaker"})[0];
-      ctrl.media_player = ctrl.stereo.component_attributes.filter(function(x){return x.category == "media_player"})[0];
+    controller: function($scope) {
+      debugger;
+      $scope.receiver = $scope.stereo.component_attributes.filter(function(x){return x.category == "receiver"})[0] || "";
+      $scope.speaker = $scope.stereo.component_attributes.filter(function(x){return x.category == "speaker"})[0] || "";
+      $scope.media_player = $scope.stereo.component_attributes.filter(function(x){return x.category == "media_player"})[0] || "";
 
     }
   }
